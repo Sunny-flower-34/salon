8cb42bf1240df6521da199edc862d96a7d378e8d
Sunny-flower-34 <sunnyflower8834@gmail.com>
2020-08-05 01:04:04 +0900

    add:routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   # get 'home/top'
   # root to: "home#top"
   root to: "home#index"
+  resources :users
+  resources :posts
+  resources :tags, only: %i[index show]
 end
