27868db5903438815d4e2707cd9d8f85b71b9576
Sunny-flower-34 <sunnyflower8834@gmail.com>
2020-08-05 19:54:03 +0900

    DB-destloy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_05_032961) do
+ActiveRecord::Schema.define(version: 2020_08_28_105006) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
