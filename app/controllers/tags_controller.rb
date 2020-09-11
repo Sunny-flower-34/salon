79ba729b673e97457df6cee84ef0deecc0f3af6f
Sunny-flower-34 <sunnyflower8834@gmail.com>
2020-09-11 18:38:34 +0900

    タグ一覧編集

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,9 +2,11 @@ class TagsController < ApplicationController
   def index
     @tags = ActsAsTaggableOn::Tag.all
     # @users = User.all
-   
+    if params[:tag_name]
       @users = User.tagged_with("#{params[:tag_name]}")
-   
+    else
+      @users = User.all
+    end
 
   end
 
