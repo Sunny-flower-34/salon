3e706965f5a464bdfd5c4e16247b1b6914021263
Sunny-flower-34 <sunnyflower8834@gmail.com>
2020-10-03 12:18:42 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,8 +5,8 @@ app_path = File.expand_path('../../', __FILE__)
 worker_processes 1
 
 #アプリケーションの設置されているディレクトリを指定
-# working_directory "#{app_path}/current/config"
-working_directory app_path
+working_directory "#{app_path}/current"
+# working_directory app_path
 
 
 #ポート番号を指定
