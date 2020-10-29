90772733514ca93e556b8e2d765d51f342e5f953
Sunny-flower-34 <sunnyflower8834@gmail.com>
2020-10-29 17:12:20 +0900

    storage :file　設定変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
+  # ↓trueで本番環境でもローカル開発環境と同じように、ブラウザにエラーメッセージが表示されるようになる
   config.consider_all_requests_local = true
   # config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
